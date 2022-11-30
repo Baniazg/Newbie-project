8e59a24fb4dceeb4330a8387937ba2695a1ca8b0
Baniazg <82947901+Baniazg@users.noreply.github.com>
2022-11-30 19:21:50 +0100

    New game
    Adding a new simple game. Enter the number and see if you hit. The computer will tell you if the number is too high or too low until you hit it.

---
+++ Guess the number/Guess the number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Guess_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, 200);
+            string welcome = "Hello, welcome in game. Guess the number betwen 0 and 200";
+            int guess = 0;
+            Console.WriteLine(welcome);
+
+
+            while (guess != number)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Too Low!!!");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too High!!!");
+                }
+
+
+            }
+            Console.WriteLine("Great. Correct answer is " + number);
+        }
+    }
+}
