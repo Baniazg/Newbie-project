1b69950b6dad20db7e972ce72fb3a76eeddc47f5
Baniazg <82947901+Baniazg@users.noreply.github.com>
2022-12-06 20:19:16 +0100

    Uptade Calculator. Add a methods.

--- NewbieApps/SimpleCalculator/Program.cs
+++ NewbieApps/SimpleCalculator/Program.cs
@@ -6,6 +6,7 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello. Welcome in simple calculator.");
 
 
@@ -16,47 +17,61 @@ namespace SimpleCalculator
             Console.WriteLine("Enter a action symbol  : [+]Addition , [-] Subtraction, [*] Multiplication, [/] Division ");
             string symbol = Console.ReadLine();
             int result = 0;
+            
 
 
-            if (symbol == "+")
+            
+            
+                if (symbol == "+")
+                {
+                    result = Sum(num1, num2);
+                }
+                else if (symbol == "-")
+                {
+                    result = Substraction(num1, num2);
+                }
+                else if (symbol == "*")
+                {
+                    result = Multiply(num1, num2);
+                }
+                else if (symbol == "/")
+                {
+                    result = Divine(num1, num2);
+                }
+                else
+                {
+                    Console.WriteLine("Havent this calculation yet. Try Again");
+                }
 
-            {
-                result = num1 + num2;
-                Console.WriteLine("Result is " + result);
-
-
-            }
-            else if (symbol == "-")
-            {
-                result = num1 - num2;
-                Console.WriteLine("Result is " + result);
-
-
-            }
-            else if (symbol == "*")
-            {
-                result = num1 * num2;
-                Console.WriteLine("Result is " + result);
-
-
-            }
-            else if (symbol == "/")
-            {
-                result = num1 / num2;
-                Console.WriteLine("Result is " + result);
-
-
-
-            }
-            else
-            {
-                Console.WriteLine("Havent this calculation yet. Try Again");
-
-            }
-
+            Console.WriteLine(num1 + symbol + num2 + " = " + result);
             Console.ReadLine();
 
+                
+            
+
         }
+        static int Sum(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+        static int Substraction(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+        static int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+        static int Divine(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
+
+        
 
 
 
