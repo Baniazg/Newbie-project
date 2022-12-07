48c221d452972890dc8b9593da54b793d3ac35e6
Baniazg <82947901+Baniazg@users.noreply.github.com>
2022-12-07 20:28:56 +0100

    Add. random name generator

---
+++ NewbieApps/NameGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstName = { "John", "Adam", "Greg", "Vin", "Jahson", "Jake", " Jack", "Harry", "Jacob", "George", "James" };
+            string[] lastName = { "Smith", "Jones", "Williams", "Diesel", "Taylor", "Davies", " Evans", "Thomas", "Johnson", "Roberts", "Wood" };
+
+            Random rand = new Random();
+            string randomFirstName = firstName[rand.Next(firstName.Length)];
+            string randomLastName = lastName[rand.Next(lastName.Length)];
+
+            Console.WriteLine("Your random name " + randomFirstName + " " + randomLastName);
+        }
+    }
+}
